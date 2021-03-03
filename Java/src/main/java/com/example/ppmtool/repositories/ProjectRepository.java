5de5208decf56f241eac079b2e19ba41e6f7a866
Skony-code <Skony-code@users.noreply.github.com>
2021-03-03 15:05:09 +0100

    Project Object modifictaions

--- Java/src/main/java/com/example/ppmtool/repositories/ProjectRepository.java
+++ Java/src/main/java/com/example/ppmtool/repositories/ProjectRepository.java
@@ -1,4 +1,12 @@
 package com.example.ppmtool.repositories;
 
-public interface ProjectRepository {
+import com.example.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository  extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
 }
